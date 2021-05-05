d5e67934206bce63f9e985e9c8f39ac2fc8f0978
amitmiran137 <amit.miran@nielsen.com>
2021-05-05 15:13:46 +0300

    fix: update action

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const fs = require("fs");
 const parse = require('doc-validator');
 
 try {
-  // `who-to-greet` input defined in action metadata file
   const markdown = core.getInput('markdown');
   const content = fs.readFileSync(markdown, "utf8");
   const result= parse(content)
